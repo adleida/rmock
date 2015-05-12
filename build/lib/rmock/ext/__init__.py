7b13963bc851d3b72472d7d62a8f3c504aef1f77
yongtao.fan <yongtao.fan@adleida.com>
2015-05-12 20:13:34 +0800

    remove buffer

--- build/lib/rmock/ext/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding:utf-8 -*-
-# File Name: rmock/ext/__init__.py
-# Author: fanyongtao
-# mail: jacketfan826@gmail.com
-# Created Time: 2015年05月11日 星期一 15时31分35秒
-#########################################################################
-
-__version__ = '0.0.1'
